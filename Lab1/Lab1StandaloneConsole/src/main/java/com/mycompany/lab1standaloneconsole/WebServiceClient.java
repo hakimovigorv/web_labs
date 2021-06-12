a28dc1c23057e31ac786801e0ff13e84f77543b4
hakimovigorv <hakimovigorv@gmail.com>
2021-06-12 22:41:42 +0300

    add async

--- Lab1/Lab1StandaloneConsole/src/main/java/com/mycompany/lab1standaloneconsole/WebServiceClient.java
+++ Lab1/Lab1StandaloneConsole/src/main/java/com/mycompany/lab1standaloneconsole/WebServiceClient.java
@@ -10,13 +10,19 @@ import com.mycompany.lab1standalone.Person;
 import com.mycompany.lab1standalone.PersonService;
 import com.mycompany.lab1standalone.FieldsInter;
 import com.mycompany.lab1standalone.FindRequest;
+import com.mycompany.lab1standalone.GetPersonsResponse;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Future;
+
 
 
 public class WebServiceClient {
-public static void main(String[] args) throws MalformedURLException {
+public static void main(String[] args) throws MalformedURLException, InterruptedException, ExecutionException {
         URL url = new URL("http://localhost:8080/PersonService?wsdl");
         PersonService personService = new PersonService(url);
         FindRequest findRequest = new FindRequest();
@@ -28,7 +34,38 @@ public static void main(String[] args) throws MalformedURLException {
 
         fieldsInterList.add(fieldsInter);
 
+        
         List<Person> persons = personService.getPersonWebServicePort().getPersons(findRequest);
+        
+        for (Person person : persons) {
+            System.out.println("name: " + person.getName() + ", surname: " + person.getSurname() + ", age: " + person.getAge() + ", birthplace: " + person.getBirthplace()+ ", university: " + person.getUniversity());
+        }
+        System.out.println("Total persons: " + persons.size());
+        
+        
+        //add tast getPersons()
+        Callable task;
+        task = () -> {
+            FindRequest findRequest2 = new FindRequest();
+            List<FieldsInter> fieldsInterList2 = findRequest2.getFields();
+            FieldsInter fieldsInter2 = new FieldsInter();
+            fieldsInter2.setField("age");
+            fieldsInter2.setValue("26");
+            fieldsInterList2.add(fieldsInter2);
+            return personService.getPersonWebServicePort().getPersons(findRequest2);
+        };
+        
+        FutureTask<List<Person>> future = new FutureTask<>(task);
+        //start task
+        new Thread(future).start();
+        //while waiting for responce do smth
+        while (!future.isDone()){
+            Thread.sleep(10);
+            System.out.println("Getting persons...");
+        }
+        
+        persons = future.get();
+        
         for (Person person : persons) {
             System.out.println("name: " + person.getName() + ", surname: " + person.getSurname() + ", age: " + person.getAge() + ", birthplace: " + person.getBirthplace()+ ", university: " + person.getUniversity());
         }
